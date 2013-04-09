ade00a460d62cba76a0531fa0fbcca7e1f6f3ea1
Fredrik Thulin <fredrik@thulin.net>
2013-04-09 15:59:11 +0200

    Add test cases for config.

---
+++ src/vccs_auth/tests/test_config.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2013, NORDUnet A/S
+# All rights reserved.
+#
+#   Redistribution and use in source and binary forms, with or
+#   without modification, are permitted provided that the following
+#   conditions are met:
+#
+#     1. Redistributions of source code must retain the above copyright
+#        notice, this list of conditions and the following disclaimer.
+#     2. Redistributions in binary form must reproduce the above
+#        copyright notice, this list of conditions and the following
+#        disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#     3. Neither the name of the NORDUnet nor the names of its
+#        contributors may be used to endorse or promote products derived
+#        from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author : Fredrik Thulin <fredrik@thulin.net>
+#
+
+"""
+Test config parsing.
+"""
+
+import os
+import unittest
+import pkg_resources
+from pprint import pprint, pformat
+
+import vccs_auth
+
+class TestCredStore(unittest.TestCase):
+
+    def setUp(self):
+        debug = False
+        datadir = pkg_resources.resource_filename(__name__, 'data')
+        self.config_file = os.path.join(datadir, 'test_config.ini')
+        self.config = vccs_auth.config.VCCSAuthConfig(self.config_file, debug)
+
+    def test_static_config(self):
+        """
+        Verify the values we have in test_config.ini
+        """
+        self.assertEqual(self.config.yhsm_device, '/dev/ttyNOSUCHDEVICE')
+        self.assertEqual(self.config.num_threads, 17)
+        self.assertEqual(self.config.add_creds_allow, ['127.0.0.127'])
+        self.assertEqual(self.config.listen_port, 8000)
