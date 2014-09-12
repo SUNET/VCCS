249cbf1a4077d4cd9f2ee345c4156b1d7e78db4a
Fredrik Thulin <fredrik@thulin.net>
2014-09-12 12:10:49 +0200

    fix error message log, variable res not defined

--- src/vccs_auth/vccs_authbackend.py
+++ src/vccs_auth/vccs_authbackend.py
@@ -452,8 +452,8 @@ class AuthBackend(object):
         try:
             res = self.hasher.safe_hmac_sha1(self.config.add_creds_password_key_handle, chr(0x0))
         except Exception:
-            self.logger.error("Failed hashing test data with add_creds_password_key_handle {!r} ({!r})".format(
-                self.config.add_creds_password_key_handle, res), traceback=True)
+            self.logger.error("Failed hashing test data with add_creds_password_key_handle {!r}".format(
+                self.config.add_creds_password_key_handle), traceback=True)
             res = 'Hashing failed'
         if len(res) >= 20:  # length of HMAC-SHA-1
             response['add_creds_hmac'] = 'OK'
